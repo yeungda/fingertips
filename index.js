f60356725c4fa87b23b88bec06af69cd5411c444
David Yeung <yeungda@gmail.com>
2013-06-07 21:51:32 +1000

    tracking differences of mouse movements

--- index.js
+++ index.js
@@ -11,16 +11,60 @@ document.addEventListener( "DOMContentLoaded",function() {
   }
   var pointHeight = 44;
   var pointWidth = 44;
-  var p = Point(0,0);
+
+  function differences(e, callback) {
+
+    var state = {
+      mouseDown: false,
+      mouseIn: false,
+      lastPosition: null
+    }
+    e.addEventListener('mousedown', function(e) {
+      state.mouseDown = true
+    })
+    e.addEventListener('mouseup', function(e) {
+      state.mouseDown = false
+      state.lastPosition = null
+    })
+    e.addEventListener('mouseout', function(e) {
+      state.mouseIn = false
+      state.lastPosition = null
+    })
+    e.addEventListener('mousein', function(e) {
+      state.mouseIn = true
+    })
+    e.addEventListener('mousemove', function(e) {
+      state.mouseIn = true
+      var p = Point(e.clientX, e.clientY)
+      if (state.mouseDown && state.mouseIn && state.lastPosition) {
+        var l = state.lastPosition;
+        var s = 1;
+        var x = p.x - l.x;
+        var y = p.y - l.y;
+        callback({
+          x: x,
+          y: y,
+          s: s
+        })
+      }
+      state.lastPosition = p;
+    })
+  }
+
+  function transform(p,d) {
+    return Point(p.x + d.x, p.y + d.y)
+  }
 
   var box = document.getElementById('box');
-  box.addEventListener('mousemove', function(e) {
-    p = Point(e.clientX, e.clientY)
+  var pointPosition = Point(200,200)
+  differences(box, function(d) {
+    console.log(d)
+    pointPosition = transform(pointPosition, d)
   })
 
   var point = document.getElementById('point')
   function render() {
-    point.setAttribute('style', 'top: ' + (p.y - (pointHeight / 2)) + 'px; left: ' + (p.x - (pointWidth / 2)) + 'px;')
+    point.setAttribute('style', 'top: ' + (pointPosition.y - (pointHeight / 2)) + 'px; left: ' + (pointPosition.x - (pointWidth / 2)) + 'px;')
     window.requestAnimationFrame(render)
   }
   render()
