945c8b63bd64dca74474a3eba1564aee5ddbb45f
David Yeung <yeungda@gmail.com>
2013-06-07 22:26:10 +1000

    mouse out too much trouble for now.

--- index.js
+++ index.js
@@ -17,37 +17,34 @@ document.addEventListener( "DOMContentLoaded",function() {
     var state = {
       mouseDown: false,
       mouseIn: false,
-      lastPosition: null
+      lastPosition: null,
     }
     e.addEventListener('mousedown', function(e) {
       state.mouseDown = true
+      state.lastPosition = Point(e.x, e.y)
+      state.xDistance = 0
     })
     e.addEventListener('mouseup', function(e) {
       state.mouseDown = false
       state.lastPosition = null
     })
-    e.addEventListener('mouseout', function(e) {
-      state.mouseIn = false
-      state.lastPosition = null
-    })
-    e.addEventListener('mousein', function(e) {
-      state.mouseIn = true
-    })
     e.addEventListener('mousemove', function(e) {
       state.mouseIn = true
-      var p = Point(e.clientX, e.clientY)
-      if (state.mouseDown && state.mouseIn && state.lastPosition) {
-        var l = state.lastPosition;
-        var s = 1;
-        var x = p.x - l.x;
-        var y = p.y - l.y;
-        callback({
-          x: x,
-          y: y,
-          s: s
-        })
+      if (state.mouseDown) {
+        var p = Point(e.x, e.y)
+        if (state.lastPosition) {
+          var l = state.lastPosition;
+          var s = 1;
+          var x = p.x - l.x;
+          var y = p.y - l.y;
+          callback({
+            x: x,
+            y: y,
+            s: s
+          })
+        }
+        state.lastPosition = p;
       }
-      state.lastPosition = p;
     })
   }
 
@@ -58,7 +55,6 @@ document.addEventListener( "DOMContentLoaded",function() {
   var box = document.getElementById('box');
   var pointPosition = Point(200,200)
   differences(box, function(d) {
-    console.log(d)
     pointPosition = transform(pointPosition, d)
   })
 
