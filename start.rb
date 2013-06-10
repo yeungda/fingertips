77ee32b4e79f1cbba0312181f63bb87adc207cbd
David Yeung <yeungda@gmail.com>
2013-06-10 19:46:56 +1000

    extracted a library

--- start.rb
+++ start.rb
@@ -5,3 +5,12 @@ get '/' do
   cache_control :no_cache
   send_file 'index.html'
 end
+
+get '/multitouch.js' do
+  s = ""
+  Dir.glob('lib/*.js').each do |f|
+    s += File.read(f)
+  end
+  content_type 'text/javascript'
+  s
+end
